0f0f3643e79fa6fc2602aea690a554c00b989fd9
monteiromaia <victormonteiromaia95@gmail.com>
2020-04-18 21:19:28 -0300

    18-04-2020

--- login-js/assets/js/login.js
+++ login-js/assets/js/login.js
@@ -5,14 +5,51 @@ console.log(email);
 console.log('Seu email é:' +email);
 
 // eventos por ID
-document.getElementById('btnsubmit').addEventListener('click', e=>{
+document.getElementById('btn-submit').addEventListener('click', e=>{
     console.log('o botão foi clicado !');
 });
 
-document.getElementById('form').addEventListener('mouseenter', e=>{
+document.getElementById('form-login').addEventListener('mouseenter', e=>{
     console.log('o botão foi clicado !');
 });
 
-document.querySelector('#form').addEventListener('mouseleave',e=>{
+document.querySelector('#form-login').addEventListener('mouseleave',e=>{
     console.log('Mouse out');
+});
+
+// submit
+
+document.querySelector('#form-login').addEventListener('submit' , e=>{
+    e.preventDefault();
+
+    let email=document.querySelector('#email').value;
+    let senha=document.querySelector('#senha').value;
+
+    console.log(email);
+    console.log(senha);
+
+    let json = {
+        email,
+        senha
+    }
+
+    console.log(json);
+
+    // converte json em string 
+
+    let stringjson = JSON.stringify(json);
+    console.log(stringjson);
+
+    // string em json
+    let parsejson = JSON.parse(stringjson);
+    console.log(parsejson);
+
+    if(!json.email){
+        console.log("vazio");
+    }else if(!json.senha){
+        console.log("vazio");
+    }else{
+        console.log("campo ok");
+    }
+
 });
