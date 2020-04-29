e0b5e54de8f9d178d4077ca77547a31153a2c38a
monteiromaia <victormonteiromaia95@gmail.com>
2020-04-28 22:08:10 -0300

    28-04-2020

--- listas/assets/js/list.js
+++ listas/assets/js/list.js
@@ -1,48 +1,68 @@
-let data = [{
-    id:1,
-    title: "Estudar HTML"
-},
-    {
-        id:2,
-        title : "Estudar Italiano"
-}];
+let data = [];
 
-function renderTodo (){
+function renderTodo() {
 
 
-
-data.forEach(task =>{
+    document.querySelector('.todo').innerHTML = '';
+    data.forEach(task => {
 
         let li = document.createElement('li');
-        li.innerHTML = 
-       
-        `<input type="checkbox" name="" id="task-${task.id}">
-        <label for="task-${task.id}">${task.title} </label>`;
+        li.innerHTML =
 
-        li.querySelector('input').addEventListener("change", e=>{
-            if(e.target.checked){
+            `<input type="checkbox" name="" id="task-${task.id}">
+        <label for="task-${task.id}">${task.title} </label>
+        <button type="button">x</button>`;
+
+
+        li.querySelector('input').addEventListener("change", e => {
+            if (e.target.checked) {
                 li.classList.add('complete');
-            }else{
+            } else {
                 li.classList.remove('complete');
             }
         });
-        
-        document.querySelector('.todo').append(li);
-})};
 
-document.querySelector('#new-task').addEventListener('keyup',e =>{
-    if(e.key === 'Enter'){
-        console.log(e.target.value);  
-        
-        data.push({
-            id:data.length+1,
-            title:e.target.value
+        li.querySelector('button').addEventListener('click', e => {
+            // console.warn(e.target);
+            // console.dir(e.target.parentNode.querySelector('input').id);
+            //console.log(e.target.parentNode.querySelector('input').id.split('-')[1]);
+
+            let button = e.target;
+            let li = button.parentNode;
+            let input = li.querySelector('input');
+            let id = input.id;
+            let idArray = id.split('-');
+            let todoId = idArray[1];
+            let title = li.querySelector('label').innerText;
+
+            if (confirm(`Deseja Realmente excluir a Tarefa ${title}`)) {
+                data = data.filter(task => task.id !== parseInt(todoId));
+
+                renderTodo();
+
+            }
+
+
+
         });
-        
-        e.target.value="";
+
+        document.querySelector('.todo').append(li);
+    });
+
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+    if (e.key === 'Enter') {
+        console.log(e.target.value);
+
+        data.push({
+            id: data.length + 1,
+            title: e.target.value
+        });
+
+        e.target.value = "";
         renderTodo();
     }
 });
 
-document.querySelector();
-// .
+renderTodo();
